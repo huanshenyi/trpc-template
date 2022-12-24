43f56601bca0179e306d130a5d13a62f33ec628a
huanshenyi <txy1226052@gmail.com>
2022-12-24 13:18:04 +0900

    フォーム表示変更

--- src/pages/settings/profile/index.tsx
+++ src/pages/settings/profile/index.tsx
@@ -1,11 +1,11 @@
-import { trpc } from '~/utils/trpc';
 import * as z from 'zod';
-import { useState } from 'react';
-import { inferProcedureInput } from '@trpc/server';
-import { Form, InputField } from '~/components/Form';
-import NextError from 'next/error';
 import { GetServerSideProps } from 'next';
+import NextError from 'next/error';
 import { useSession, getSession } from 'next-auth/react';
+import { inferProcedureInput } from '@trpc/server';
+
+import { trpc } from '~/utils/trpc';
+import { Form, InputField } from '~/components/Form';
 import { NextPageWithLayout } from '~/pages/_app';
 import { Layout } from '~/features/settings/components';
 import { Button } from '~/components/Elements/Button';
@@ -35,14 +35,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const ProfilePage: NextPageWithLayout = () => {
-  const [showForm, setShowForm] = useState<boolean>(false);
-  const [newName, setNewName] = useState<string>('');
   const { data: session } = useSession();
   const utils = trpc.useContext();
 
   const fixUser = trpc.user.fixById.useMutation({
     async onSuccess() {
-      setNewName('');
       await utils.user.byId.invalidate();
     },
   });
@@ -78,90 +75,56 @@ const ProfilePage: NextPageWithLayout = () => {
                 <div className="form-control w-52">
                   <label className="cursor-pointer label">
                     <span className="label-text font-extrabold">基本情報</span>
-                    <Button
-                      size="sm"
-                      variant="info"
-                      onClick={() => {
-                        setShowForm(!showForm);
-                      }}
-                    >
-                      <svg
-                        className="w-5 h-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                        ></path>
-                      </svg>
-                      編集
-                    </Button>
                   </label>
                 </div>
               </div>
               <div className="divider mt-0"></div>
               <div className="overflow-x-auto">
-                {showForm ? (
-                  <Form<createValues, typeof schema>
-                    onSubmit={async (values) => {
-                      type Input = inferProcedureInput<
-                        AppRouter['user']['fixById']
-                      >;
-                      const input: Input = {
-                        id: session?.user.id as string,
-                        name: values.name,
-                      };
-                      try {
-                        await fixUser.mutateAsync(input);
-                      } catch (cause) {
-                        console.error({ cause }, 'Failed to add post');
-                      }
-                    }}
-                    schema={schema}
-                    options={{
-                      shouldUnregister: true,
-                      defaultValues: {
-                        name: data.name as string,
-                      },
-                    }}
-                    className="m-auto text-center"
-                  >
-                    {({ register, formState }) => (
-                      <>
-                        <InputField
-                          type="text"
-                          label="場所を追加"
-                          error={formState.errors['name']}
-                          registration={register('name')}
-                          className="lg:w-3/5 m-auto"
-                        />
-                        <div>
-                          <Button
-                            isLoading={false}
-                            type="submit"
-                            className="m-auto text-center lg:w-3/5 w-full flex justify-center border-indigo-600 bg-transparent dark:bg-sky-500 dark:text-white p-4 border rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline shadow-lg cursor-pointer transition ease-in duration-300"
-                          >
-                            募集追加
-                          </Button>
-                        </div>
-                      </>
-                    )}
-                  </Form>
-                ) : (
-                  <table className="table w-full">
-                    <tbody>
-                      <tr>
-                        <th>名前</th>
-                        <td>{data.name}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                )}
+                <Form<createValues, typeof schema>
+                  onSubmit={async (values) => {
+                    type Input = inferProcedureInput<
+                      AppRouter['user']['fixById']
+                    >;
+                    const input: Input = {
+                      id: session?.user.id as string,
+                      name: values.name,
+                    };
+                    try {
+                      await fixUser.mutateAsync(input);
+                    } catch (cause) {
+                      console.error({ cause }, 'Failed to add post');
+                    }
+                  }}
+                  schema={schema}
+                  options={{
+                    shouldUnregister: true,
+                    defaultValues: {
+                      name: data.name as string,
+                    },
+                  }}
+                  className="m-auto text-center"
+                >
+                  {({ register, formState }) => (
+                    <>
+                      <InputField
+                        type="text"
+                        label="場所を追加"
+                        error={formState.errors['name']}
+                        registration={register('name')}
+                        className="lg:w-3/5 m-auto"
+                      />
+                      <div>
+                        <Button
+                          isLoading={false}
+                          type="submit"
+                          className="m-auto text-center lg:w-3/5 w-full flex justify-center border-indigo-600 bg-transparent dark:bg-sky-500 dark:text-white p-4 border rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline shadow-lg cursor-pointer transition ease-in duration-300"
+                        >
+                          募集追加
+                        </Button>
+                      </div>
+                    </>
+                  )}
+                </Form>
               </div>
             </div>
           </div>
