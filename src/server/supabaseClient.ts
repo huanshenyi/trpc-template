f11a8db397667b1083d80a6c01a8485f7283ea58
huanshenyi <txy1226052@gmail.com>
2023-01-02 18:04:40 +0900

    env追加

--- src/server/supabaseClient.ts
+++ src/server/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-console.log('supabaseUrl', supabaseUrl);
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
